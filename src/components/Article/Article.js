4b7f8608be642b342f2ac512c8ab1187067384a0
Victor Pavlov <vitekpavlov@gmail.com>
2019-08-23 18:31:22 +0300

    Clear code formating

--- src/components/Article/Article.js
+++ src/components/Article/Article.js
@@ -5,17 +5,15 @@ import ArticleBody from './ArticleBody';
 const Article = ({
   content: {
     title,
-    text: {
-      json
-    },
-    publishedAt
-  }
+    text: { json },
+    publishedAt,
+  },
 }) => (
   <div>
     <h2>{title}</h2>
     <ArticleBody content={json} />
     <h5>{publishedAt}</h5>
   </div>
-)
+);
 
-export default Article
+export default Article;
--- src/components/Article/ArticleBody.js
+++ src/components/Article/ArticleBody.js
@@ -1,21 +1,24 @@
 import React from 'react';
 import { documentToReactComponents } from '@contentful/rich-text-react-renderer';
-import { BLOCKS } from '@contentful/rich-text-types'
+import { BLOCKS } from '@contentful/rich-text-types';
 
-const ArticleBody = (props) => {
+const ArticleBody = props => {
   const options = {
     renderNode: {
-      [BLOCKS.EMBEDDED_ASSET]: (node) => {
+      [BLOCKS.EMBEDDED_ASSET]: node => {
         console.log(node);
         const obj = node.data.target;
-        return <img src={obj.fields.file['en-US'].url} alt={obj.fields.description['en-US']} />;
-      }
-    }
+        return (
+          <img
+            src={obj.fields.file['en-US'].url}
+            alt={obj.fields.description['en-US']}
+          />
+        );
+      },
+    },
   };
   const articleBody = documentToReactComponents(props.content, options);
-  return (
-    <div>{articleBody}</div>
-  );
+  return <div>{articleBody}</div>;
 };
 
 export default ArticleBody;
